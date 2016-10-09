255a659de110eaa6175518a469842f646a4d3ce0
Jack Flannery <jmflannery81@gmail.com>
2016-10-09 17:00:35 -0400

    Some updates
    - jade -> pug
    - minifycss -> cleancss
    - npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp'),
-    jade = require('gulp-jade'),
+    pug = require('gulp-pug'),
     uglify = require('gulp-uglify'),
     less = require('gulp-less'),
     autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
+    cleancss = require('gulp-clean-css'),
     concat = require('gulp-concat'),
     imagemin = require('gulp-imagemin'),
     sourcemaps = require('gulp-sourcemaps'),
     clean = require('gulp-clean'),
     connect = require('gulp-connect');
+    runSequence = require('run-sequence');
 
 gulp.task('clean_html', function() {
   return gulp.src('build/*.html')
@@ -46,8 +47,8 @@ gulp.task('clean', function() {
 });
 
 gulp.task('templates', ['clean_html'], function() {
-  return gulp.src('./templates/**/*.jade')
-    .pipe(jade())
+  return gulp.src('./templates/**/*.pug')
+    .pipe(pug())
     .pipe(gulp.dest('./build'));
 });
 
@@ -59,12 +60,14 @@ gulp.task('less', ['clean_css'], function() {
 });
 
 gulp.task('css', ['minify_css'], function() {
-  gulp.run('clean_temp');
+  // gulp.run('clean_temp');
+  return gulp.src('temp')
+    .pipe(clean());
 });
 
 gulp.task('minify_css', ['less'], function() {
   return gulp.src(['./css/**/*.css', './temp/css/**/*.css'])
-    .pipe(minifycss())
+    .pipe(cleancss())
     .pipe(concat('style.min.css'))
     .pipe(gulp.dest('./build/css'));
 });
@@ -88,7 +91,7 @@ gulp.task('fonts', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./templates/**/*.jade', ['templates']);
+  gulp.watch('./templates/**/*.pug', ['templates']);
   gulp.watch('./less/**/*.less', ['css']);
   gulp.watch('./js/**/*.js', ['scripts']);
   gulp.watch('./images/**/*', ['images']);
@@ -102,14 +105,17 @@ gulp.task('webserver', function() {
   });
 });
 
-gulp.task('default', ['clean'], function() {
-  gulp.run(
-    'css',
-    'scripts',
-    'images',
-    'fonts',
-    'templates',
-    'watch',
-    'webserver'
+gulp.task('default', function() {
+  runSequence(
+    'clean',
+    [
+      'css',
+      'scripts',
+      'images',
+      'fonts',
+      'templates',
+      'watch',
+      'webserver'
+    ]
   );
 });
